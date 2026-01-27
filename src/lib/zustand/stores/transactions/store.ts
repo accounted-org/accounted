7c04c16e66e79800db46c877b37281c3a65b5c0d
Pedro Braghin <braghindev@gmail.com>
2026-01-26 22:58:25 -0300

    fix(i18n): adjust missing messages and add test link to test page

--- src/lib/zustand/stores/transactions/store.ts
+++ src/lib/zustand/stores/transactions/store.ts
@@ -4,7 +4,15 @@ import { TransactionStore } from './types';
 import { transactionActions } from './actions';
 
 export const transactionStore = createStore<TransactionStore>((set, get) => ({
-  transactions: [],
+  transactions: [
+    {
+      amount: 100,
+      category: 'Groceries',
+      date: '2026-01-27T01:56:04.513Z',
+      id: '1',
+      source: 'Supermarket',
+    },
+  ],
   ...transactionActions(set, get),
 }));
 
